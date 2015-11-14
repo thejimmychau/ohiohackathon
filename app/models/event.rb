cbd3bec20bff00183db73a90b515edbb23be38b1
Jimmy Chau <thejimmychau@gmail.com>
2015-11-14 17:47:28 -0500

    Added validations to Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
-class Event < ActiveRecord::Base    
-    attr_accessor :address      #Model 'method' for getting address. This is simply a reference to a view form field's value
+class Event < ActiveRecord::Base   
+	validates :title, presence: true, length: { maximum: 50 }
+	validates :description, presence: true, length: { maximum: 50 } 
+	validates :person_count_cap, numericality: { only_integer: true }
+    # attr_accessor :address      #Model 'method' for getting address. This is simply a reference to a view form field's value
     
     belongs_to :user
     has_many :attends
