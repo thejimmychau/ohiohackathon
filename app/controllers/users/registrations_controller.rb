0aba84072ba0484090621500a255a51eef8deb42
James Lowrey <jtronlabs@gmail.com>
2015-11-14 21:22:09 -0500

    sign-ups are working

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -39,15 +39,12 @@ before_filter :configure_account_update_params, only: [:update]
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.for(:sign_up) << :attribute
-  # end
+    def configure_sign_up_params
+       devise_parameter_sanitizer.for(:sign_up) << [:first_name,:last_name]
+   end
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_account_update_params
-       devise_parameter_sanitizer.for(:account_update)<<[:first_name,:last_name]
-   end
-   def configure_sign_up_params
        devise_parameter_sanitizer.for(:account_update)<<[:first_name,:last_name]
    end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,17 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
+	     :recoverable, :rememberable, :trackable, :validatable
+    
+    
+    
 	validates :first_name, presence: true, length: { maximum: 50 }
 	validates :last_name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 }, 
 		format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 	validates :password, presence: true, length: {minimum: 8}
-
-	devise :database_authenticatable, :registerable,
-	     :recoverable, :rememberable, :trackable, :validatable
 
 	has_many :events
 	has_many :attends
