6c76be3416bbff0ecd9e607ba777fbdd67a42bae
James Lowrey <jtronlabs@gmail.com>
2015-11-15 00:04:39 -0500

    improved event forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,38 +25,48 @@ billy = User.create!(
 
 Event.delete_all
 
-Event.create!( #nearby event, location = Ohio Union
-    title: "test-near",
+Event.create!( #nearby event, location = Ohio Union  
+    title: "test-near",  
     start_time:DateTime.now,
     end_time:DateTime.now+100,
     event_tag_id:sportTagId,
     description: "lots of fun!",
     person_count_cap:10,
-    user_id:billy.id,
+    user_id:billy.id, 
+    
+    :address => "Ohio Union",
+    state: "Ohio",
+    city: "Columbus",
+    zip_code:43210,
     latitude: 39.9980611,
     longitude:-83.00892859999999
     )
 
-Event.create!( #nearby event, location = Ohio Union
-    title: "test-2",
-    start_time:Time.now,
-    end_time:Time.now+10000000,
-    event_tag_id:20,
-    description: "extremely fun!",
-    person_count_cap:10,
-    user_id:20,
-    latitude: 40.0017,
-    longitude:-83.0197
-    )
+ Event.create!( #nearby event, location = Ohio Union  
+   title: "test-2",     
+     start_time:Time.now,     
+     end_time:Time.now+10000000,     
+     event_tag_id:sportTagId,     
+     description: "extremely fun!",     
+     person_count_cap:10,     
+     user_id:billy.id,          
+     address:"Ohio Union",     
+     state:"Ohio",     
+     city:"Columbus",     
+     zip_code:43210,     
+     latitude: 40.0017,     
+     longitude:-83.0197     )  
 
-Event.create!( #far away event 
-    title: "test-far",
-    start_time:DateTime.now,
-    end_time:DateTime.now+10,
-    event_tag_id:sportTagId,
-    description: "lots of fun!",
-    person_count_cap:10,
-    user_id:billy.id,
-    latitude: 0,
-    longitude:0
-    )
+Event.create!( #far away event      
+    title: "test-far",     
+    start_time:DateTime.now,     
+    end_time:DateTime.now+10,     
+    event_tag_id:sportTagId,     
+    description: "lots of fun!",     
+    person_count_cap:10,     
+    user_id:billy.id,          
+    address:"5839 Westheimer Rd",     
+    state:"Texas",     
+    city:"Houston",     
+    latitude: 29.737939,     
+    longitude: -95.482203     ) 
